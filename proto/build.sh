c0e0b569e0b6291b5e2556c33d5e3729ceae8ad3
W3AXL <29879554+W3AXL@users.noreply.github.com>
2026-09-22 17:03:58 -0500

    added TS build to proto

--- proto/build.sh
+++ proto/build.sh
@@ -3,8 +3,15 @@
 # Location of the current script
 PROTO_DIR=$(dirname $realpath $0)
 
+# Location of the console directory for TS generation
+CONSOLE_DIR=$(realpath "../../../console")
+
 # Compile C#
 protoc --proto_path=$PROTO_DIR --csharp_out="$PROTO_DIR/../" "$PROTO_DIR"/RC2Proto.proto
 
-# Compile TS
-# TODO
+# Compile TS/Node
+protoc --plugin=protoc-gen-ts_proto=$CONSOLE_DIR/node_modules/.bin/protoc-gen-ts_proto.cmd \
+    --ts_proto_out=$CONSOLE_DIR/generated \
+    --ts_proto_opt=esModuleInterop=true \
+    --ts_proto_opt=forceLong=bigint \
+    -I "$PROTO_DIR" RC2Proto.proto
